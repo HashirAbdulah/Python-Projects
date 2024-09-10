3943f41506c71374a40484a47b15106c6caa86bf
Hashir Abdullah <109906333+HashirAbdulah@users.noreply.github.com>
2024-09-10 17:52:06 +0500

    Added

--- Email-ValidatorUsingRegix.py
+++ Email-ValidatorUsingRegix.py
@@ -1,7 +1,7 @@
 import re
 
 
-pattern =  r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+pattern =  r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
 
 email = input("Enter Email Adress: ")
 
